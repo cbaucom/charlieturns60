731abe03f7697b6dffd9a1159ae17237c4614c66
Chris Baucom <crbaucom@gmail.com>
2019-08-21 23:57:25 -0400

    Adding analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,18 @@ module.exports = {
         icon: `src/images/ceb-kid-headshot.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "206887009",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
