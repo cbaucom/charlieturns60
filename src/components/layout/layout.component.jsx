48b96388577868404bc281f0654bf195cb41c321
Chris Baucom <crbaucom@gmail.com>
2019-08-22 15:36:41 -0400

    Trying to fix IE issues with Map

--- src/components/layout/layout.component.jsx
+++ src/components/layout/layout.component.jsx
@@ -25,7 +25,8 @@ const Layout = ({ children }) => (
     render={data => (
       <>
         <Helmet>
-          <script src="https://cdn.polyfill.io/v2/polyfill.js?features=default,Symbol" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.35.5/es6-shim.min.js" />
+          <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces6%2Ces7%2Ces5%2CArray.from%2CMap%2CObject.keys%2CObject.values%2CSet%2CSymbol%2Cfetch%2Cnavigator.geolocation%2CrequestAnimationFrame" />
           <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.min.js"></script>
           <script src="https://cdn.jsdelivr.net/npm/es6-promise@4/dist/es6-promise.auto.min.js"></script>
         </Helmet>
